91956f8ae2627d447c64a3bb77bc988b19b444ab
jerry <10166007@ntub.edu.tw>
2025-07-02 17:00:52 +0800

    feat：use google cloud storage function
    poetry add google-cloud-storage

--- llm/main.py
+++ llm/main.py
@@ -40,6 +40,9 @@ from .routes import auth, files, users, admin
 from .log import init_logging
 log = init_logging()
 
+### 執行測試檔 ###
+# from llm.utils.test import *
+
 # from .transcribe_speech import pipe
 # print(pipe)
 
@@ -64,7 +67,6 @@ from .database import Base, engine
 # from .models import Base
 
 from llm.env import BASE_DIR
-
 
 # 透過 Depends 注入 db，建立 Session
 # 一個 db 的 dependency，可以看做是要操作的 db，這裡的 Depends 對應 get_db， get_db 對應 SessionLocal
